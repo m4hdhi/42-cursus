ab0f637606cb1947ce964863ac4511f83493981a
Mahdhi Muzammil <mamuzamm@lab1r3s11.42abudhabi.ae>
2025-06-25 22:38:07 +0400

    submit

---
+++ client_bonus.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamuzamm <mamuzamm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 21:49:58 by mamuzamm          #+#    #+#             */
+/*   Updated: 2025/06/25 22:25:18 by mamuzamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk_bonus.h"
+
+void	confirm_msg(int signal)
+{
+	if (signal == SIGUSR2)
+		write(1, "message recieved\n", 17);
+}
+
+static int	ft_atoi(const char *str)
+{
+	int					i;
+	int					sign;
+	unsigned long int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+void	ft_atob(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (0x01 << bit)))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(500);
+		bit++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		if (pid <= 0)
+			return (write(1, "Error: Invalid PID\n", 19), 1);
+		while (argv[2][i] != '\0')
+		{
+			ft_atob(pid, argv[2][i]);
+			i++;
+		}
+		signal(SIGUSR2, confirm_msg);
+		ft_atob(pid, '\0');
+	}
+	else
+	{
+		write(1, "Error\n", 6);
+		return (1);
+	}
+	return (0);
+}
